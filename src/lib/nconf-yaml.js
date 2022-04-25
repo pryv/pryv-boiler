dac64637d2ca2ce049b593813df3a1ae5f3b925f
Simon Goumaz <simon@attentif.ch>
2022-04-25 14:37:47 +0200

    Use latest `source-licenser`

--- src/lib/nconf-yaml.js
+++ src/lib/nconf-yaml.js
@@ -1,8 +1,6 @@
 /**
  * @license
- * Copyright (C) 2020-2022 Pryv S.A. https://pryv.com 
- * This file is part of Open-Pryv.io and released under BSD-Clause-3 License
- * SPDX-License-Identifier: BSD-3-Clause
+ * [BSD-3-Clause](https://github.com/pryv/pryv-boiler/blob/master/LICENSE)
  */
 const yaml = require('js-yaml');
 
